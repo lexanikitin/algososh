57017b37f89d3b272c4527a303be5a6ba26e8878
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-05-20 18:28:06 +0300

    feat: верстка страницы Связный список

--- src/components/list-page/list-page.tsx
+++ src/components/list-page/list-page.tsx
@@ -1,10 +1,77 @@
 import React from "react";
-import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import {SolutionLayout} from "../ui/solution-layout/solution-layout";
+import styles from "./list-page.module.css";
+import {Input} from "../ui/input/input";
+import {Button} from "../ui/button/button";
+import {Circle} from "../ui/circle/circle";
 
 export const ListPage: React.FC = () => {
   return (
     <SolutionLayout title="Связный список">
+      <div className={styles.wrapper}>
+        <div className={styles.taskContainer}>
+          <div className={styles.controlRow}>
+            <Input
+              type={'text'}
+              maxLength={4}
+              isLimitText={true}
 
+
+            />
+            <div className={styles.buttonWrapper}>
+              <Button
+                type={'button'}
+                text={'Добавить в head'}
+                linkedList={'big'}
+
+
+              />
+              <Button
+                type={'button'}
+                text={'Добавить в tail'}
+                linkedList={'big'}
+
+              />
+              <Button
+                type={'button'}
+                text={'Удалить из head'}
+                linkedList={'big'}
+
+              />
+              <Button
+                type={'button'}
+                text={'Удалить из tail'}
+                linkedList={'big'}
+
+              />
+            </div>
+          </div>
+          <div className={styles.controlRow}>
+            <Input
+              type={'number'}
+              max={19}
+              isLimitText={true}
+
+
+            />
+            <div className={styles.buttonWrapper}>
+              <Button
+                type={'button'}
+                text={'Добавить по индексу'}
+                linkedList={'big'}
+
+              />
+              <Button
+                type={'button'}
+                text={'Удалить по индексу'}
+                linkedList={'big'}
+
+              />
+            </div>
+          </div>
+        </div>
+
+      </div>
     </SolutionLayout>
   );
 };
