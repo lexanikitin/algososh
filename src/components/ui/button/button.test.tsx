c3b8ca3ac3da2fa4828c8ded98610cf056069309
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-06-16 13:41:43 +0300

    feat: unit тестирование компонента Button, тест callback

--- src/components/ui/button/button.test.tsx
+++ src/components/ui/button/button.test.tsx
@@ -1,29 +1,37 @@
 import {Button} from "./button";
 import renderer from "react-test-renderer";
+import {fireEvent, render, screen} from "@testing-library/react";
 
 describe('Отрисовка компонента Button:', () => {
-  test('кнопка с текстом', () => {
+  it('кнопка с текстом', () => {
     const testTree = renderer
       .create(<Button text={'Test'}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('кнопка без текста', () => {
+  it('кнопка без текста', () => {
     const testTree = renderer
       .create(<Button/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('заблокированная кнопка', () => {
+  it('заблокированная кнопка', () => {
     const testTree = renderer
       .create(<Button text={'Test'} disabled={true}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('кнопка с индикацией загрузки', () => {
+  it('кнопка с индикацией загрузки', () => {
     const testTree = renderer
       .create(<Button text={'Test'} isLoader={true}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
+  it('корректность вызова callback при клике', ()=>{
+    const callback = jest.fn();
+    render(<Button onClick={callback}/>);
+    fireEvent.click(screen.getByRole('button'))
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
 })
--- src/components/ui/circle/circle.test.tsx
+++ src/components/ui/circle/circle.test.tsx
@@ -3,67 +3,67 @@ import renderer from "react-test-renderer";
 import {ElementStates} from "../../../types/element-states";
 
 describe('Отрисовка компонента Circle:', () => {
-  test('без буквы', () => {
+  it('без буквы', () => {
     const testTree = renderer
       .create(<Circle/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('с буквами', () => {
+  it('с буквами', () => {
     const testTree = renderer
       .create(<Circle letter={'test'}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('с head', () => {
+  it('с head', () => {
     const testTree = renderer
       .create(<Circle head={'test'}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('с react-элементом в head', () => {
+  it('с react-элементом в head', () => {
     const testTree = renderer
       .create(<Circle head={<Circle/>}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('с tail', () => {
+  it('с tail', () => {
     const testTree = renderer
       .create(<Circle tail={'test'}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('с react-элементом в tail', () => {
+  it('с react-элементом в tail', () => {
     const testTree = renderer
       .create(<Circle tail={<Circle/>}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('с index', () => {
+  it('с index', () => {
     const testTree = renderer
       .create(<Circle index={1}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('с пропом isSmall ===  true', () => {
+  it('с пропом isSmall ===  true', () => {
     const testTree = renderer
       .create(<Circle isSmall={true}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('в состоянии Default', () => {
+  it('в состоянии Default', () => {
     const testTree = renderer
       .create(<Circle state={ElementStates.Default}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('в состоянии Changing', () => {
+  it('в состоянии Changing', () => {
     const testTree = renderer
       .create(<Circle state={ElementStates.Changing}/>)
       .toJSON();
     expect(testTree).toMatchSnapshot();
   });
-  test('в состоянии Modified', () => {
+  it('в состоянии Modified', () => {
     const testTree = renderer
       .create(<Circle state={ElementStates.Modified}/>)
       .toJSON();
