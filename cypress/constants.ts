9e8736113a7e88de19e47b36d988d680f6b4e0cb
Alexey Nikitin <lexa.nikitin@gmail.com>
2023-06-18 12:27:13 +0300

    feat: e2e тест роутинга по страницам приложения

--- cypress/constants.ts
+++ cypress/constants.ts
@@ -1,1 +1,8 @@
-export const BASE_URL = 'http://localhost:3000'
+export const MAIN_PAGE_URL = 'http://localhost:3000'
+
+export const STRING_URL = '/recursion';
+export const FIBONACCI_URL = '/fibonacci';
+export const SORTING_URL = '/sorting';
+export const STACK_URL = '/stack';
+export const QUEUE_URL = '/queue';
+export const LIST_URL = '/list';
--- cypress/e2e/is-available.spec.cy.js
+++ cypress/e2e/is-available.spec.cy.js
@@ -1,8 +1,8 @@
-import {BASE_URL} from "../constants";
+import {MAIN_PAGE_URL} from "../constants";
 
 describe('Запуск приложения', ()=>{
   it('приложение поднялось', ()=>{
-    cy.visit(BASE_URL);
+    cy.visit(MAIN_PAGE_URL);
     cy.contains(/МБОУ АЛГОСОШ/i);
   })
 })
